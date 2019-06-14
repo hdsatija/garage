6ddd686e8fab5413630a597149173d44892d638e
hdsatija <hunnysatija01@gmail.com>
2019-06-14 16:58:37 +0530

    home page

--- feedback/src/app/app.module.ts
+++ feedback/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GoodComponent } from './good/good.component';
+import { BadComponent } from './bad/bad.component';
+import { GoogleformComponent } from './bad/googleform/googleform.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GoodComponent,
+    BadComponent,
+    GoogleformComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
